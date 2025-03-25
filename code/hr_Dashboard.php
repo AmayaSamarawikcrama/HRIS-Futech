ecf8e82a0de35dd991905594f465559f0ec863cf
BHdahanayaka2003 <budhvinhimsadadahanayaka@gmail.com>
2025-03-25 22:58:52 +0530

    Update

--- code/hr_Dashboard.php
+++ code/hr_Dashboard.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
-    <link rel="stylesheet" href="CSS/hr_Dashboard.css">
+    <link rel="stylesheet" href="hr_Dashboard.css">
 </head>
 <body>
     <div class="container">
