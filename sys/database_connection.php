f845ab18e117ca220197e82ac65140459650c49f
HesaraDinal1234 <hesaradinal48@gmail.com>
2025-03-26 13:31:20 +0530

    updated_dashboard

--- sys/database_connection.php
+++ sys/database_connection.php
@@ -1,12 +1,14 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "hris_db";
+$servername = "localhost";  // Database server (e.g., localhost or IP)
+$db_username = "root";      // Database username
+$db_password = "";          // Database password (for XAMPP, the default is usually empty)
+$dbname = "hris_db";        // Your database name
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Create connection
+$conn = new mysqli($servername, $db_username, $db_password, $dbname);
 
-if ($conn->connect_error) 
-{
+// Check connection
+if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+?>
